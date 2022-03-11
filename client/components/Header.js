a9f7444b6c16e7889609643f2b7f1f997410a413
Diogo Sales Teixeira <diogoteixeira0602@gmail.com>
2022-03-11 14:39:06 -0300

    header stylized

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,7 +6,20 @@ import { HiOutlineDotsVertical } from 'react-icons/hi'
 import ethLogo from '../assets/eth.png'
 import uniswapLogo from '../assets/uniswap.png'
 
-const style = {}
+const style = {
+  wrapper: `p-4 w-screen flex justify-between items-center`,
+  headerLogo: `flex w-1/4 items-center justify-start`,
+  nav: `flex-1 flex justify-center items-center`,
+  navItemsContainer: `flex bg-[#191B1F] rounded-3xl`,
+  navItem: `px-4 py-2 m-1 flex items-center text-lg font-semibold text-[0.9rem] cursor-pointer rounded-3xl`,
+  activeNavItem: `bg-[#20242A]`,
+  buttonsContainer: `flex w-1/4 justify-end items-center`,
+  button: `flex items-center bg-[#191B1F] rounded-2xl mx-2 text-[0.9rem] font-semibold cursor-pointer`,
+  buttonPadding: `p-2`,
+  buttonTextContainer: `h-8 flex items-center`,
+  buttonIconContainer: `flex items-center justify-center w-8 h-8`,
+  buttonAccent: `bg-[#172A42] border border-[#163256] hover:border-[#234169] h-full rounded-2xl flex items-center justify-center text-[#4F90EA]`,
+}
  
 const Header = () => {
   const [selectedNav, setSelectedNav] = useState('swap')
