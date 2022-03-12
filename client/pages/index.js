c1a2d34aabb02a6b43d2ae31447bb67f8c8abec1
Diogo0602x <diogoteixeira0602@gmail.com>
2022-03-12 00:24:48 -0300

    Main done

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head' 
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 const style = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -9,7 +10,7 @@ export default function Home() {
   return(
     <div className={style.wrapper}>
       <Header/>
-      <h2>Main</h2>
+      <Main/>
       <h2>Transaction History</h2>
     </div>
   ) 
